2620ccdb1466d71eca8b3dc325a6cc79359f5253
Nameuser9 <112658205+Nameuser9@users.noreply.github.com>
2022-09-05 13:56:29 +0700

    Сделал вторую задачу

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Введите 3 числа");
+double a = Convert.ToDouble(Console.ReadLine()) , b = Convert.ToDouble(Console.ReadLine()) , c = Convert.ToDouble(Console.ReadLine());
+double max = a;
+if (a > b) 
+{
+    max = a;
+}
+else
+{
+    max = b;
+}
+if (max > c)
+{
+    Console.Write (max);
+}
+else
+{
+    max = c;
+    Console.Write (max);
+}
+Console.Write (" - самое большое число");
